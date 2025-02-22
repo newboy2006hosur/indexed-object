9548426c06a910b5e4cd71a148423233b8d0992f
Manjunath R <manjunath.r@knownshare.in>
2025-02-22 17:12:31 +0530

    Initial commit from your app

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
